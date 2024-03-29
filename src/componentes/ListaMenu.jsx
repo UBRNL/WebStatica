d88a75638a7db3f5edb6c026e58d38a0a2fbaecb
Ubernel <hubernelh@gmail.com>
2024-03-29 15:44:00 -0500

    navegaciony funcion LiastaMenu

--- src/componentes/ListaMenu.jsx
+++ src/componentes/ListaMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ListaMenu() {
     const [mostrarLista, setMostrarLista] = useState(false);
@@ -18,11 +19,11 @@ export default function ListaMenu() {
             {mostrarLista && (
             <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-gray-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                 <div className="py-1" role="none">
-                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Indice</a>
-                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Acerca</a>
-                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Servicio</a>
-                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Producto</a>
-                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Contacto</a>
+                <Link to="/" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Indice</Link>
+                <Link to="/about" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Acerca</Link>
+                <Link to="/servicios" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Servicio</Link>
+                <Link to="/productos" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Producto</Link>
+                <Link to="/contacto" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Contacto</Link>
                 </div>
             </div>)}
         </div>
